90415545c3c44fb88a1f3aadb7df2ca142313428
Mohamed Mujtaba <mohamedmujtaba07@gmail.com>
2024-09-25 23:42:21 +0530

    updated api urls

--- clients/api.ts
+++ clients/api.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
-export const graphqlClient = new GraphQLClient("https://d1xbuwle301j3p.cloudfront.net/graphql",{
+export const graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_X_CLONE_API_URL as string,{
     headers: () => ({
       Authorization: isClient
         ? `Bearer ${window.localStorage.getItem("__twitter_token")}`
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 };
 export default config;
